c1f7c0a19019331d1cc2008d03279d74c0c96da3
Rostem Bassalow <>
2021-01-06 21:20:04 -0800

    01-06-21 9pm

--- MapProfiles/settings.py
+++ MapProfiles/settings.py
@@ -23,13 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 with open(os.path.join(BASE_DIR, 'secret_key.txt') ) as f:
     SECRET_KEY = f.read().strip()
-    print(SECRET_KEY[-4:])
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
 #DEBUG = False
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
-print(f'{DEBUG=}')
 
 #ALLOWED_HOSTS = []
 
@@ -130,7 +128,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 # The absolute path to the directory where collectstatic will collect static files for deployment.
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'  #. os.path.join(BASE_DIR, 'staticfiles')
 
 # The URL to use when referring to static files (where they will be served from)
 STATIC_URL = '/static/'
