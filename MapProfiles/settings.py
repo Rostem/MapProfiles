dfcbc75cdb461463ead14b46c2b958810d25cd44
Rostem Bassalow <>
2021-01-10 11:25:26 -0800

    0103 1125am

--- MapProfiles/settings.py
+++ MapProfiles/settings.py
@@ -21,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+#SECRET_KEY = os.environ['SECRET_KEY']
+with open(os.path.join(BASE_DIR, 'secret_key.txt') ) as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
